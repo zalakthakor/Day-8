b0f7a5e0a6760da4b2d0bc78fdcd7c410288b8b0
ZALAK THAKOR <72291815+zalakthakor@users.noreply.github.com>
2021-06-04 16:10:13 -1200

    Add files via upload

---
+++ edit.php
@@ -0,0 +1,38 @@
+<?php
+$connection=mysqli_connect("localhost","root","","db_intern");
+if(!isset($_GET['id']) || empty($_GET['id'])){
+    header("location:table.php");
+    }
+    $editid=$_GET['id'];
+    $editq=mysqli_query($connection,"select * from tbl_user where user_id='{$editid}'") or 
+    die(mysqli_error($connection));
+    $editdata=mysqli_fetch_array($editq);
+    if($_POST){
+        $txt1=$_POST['txt1'];
+        $txt2=$_POST['txt2'];
+        $txt3=$_POST['txt3'];
+    $uq=mysqli_query($connection,"update tbl_user set user_name='{$txt1}',user_gender='{$txt2}',
+    user_mobile='{$txt3}' where user_id='{$editid}'") or die(mysqli_error($connection));
+    if($uq){
+        echo "<script>alert('Record Updated');window.location='table.php';</script>";
+    }
+}
+?>
+<html>
+<body>
+<form method="POST">
+    Name:<input type="text" value="<?php echo $editdata['user_name'];?>"
+name="txt1"/>
+Gender : Male<input type="radio"
+<?php if($editdata['user_gender']=="Male"){echo "checked";}?>
+value="Male" name="txt2"/>
+Female<input type="radio"
+<?php if($editdata['user_gender']=="Female"){echo "checked";}?>
+ name="txt2"/>
+Mobile : <input type="number" value="<?php echo $editdata['user_mobile'];?>"
+name="txt3"/>
+<br/>
+<input type="submit"/>
+</form>
+</body>
+</html>
